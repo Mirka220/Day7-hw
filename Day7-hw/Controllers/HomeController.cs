91a66429da702373024400f958c96efe4e9794f5
Mirka220 <mirka.gamer@mail.ru>
2022-10-27 11:14:02 +0600

    first commit

---
+++ Day7-hw/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Day7_hw.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace Day7_hw.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public JsonResult GetJson()
+        {
+            //string FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files/users.json");
+            //var jsonOptions = new System.Text.Json.JsonSerializerOptions
+            //{
+            //    PropertyNameCaseInsensitive = true,
+            //    WriteIndented = true
+            //};
+            //return Json(FilePath);
+
+            using (FileStream fs = new FileStream(@"C:\Users\Home\source\repos\Day7-hw\Day7-hw\Files\users.json", FileMode.OpenOrCreate))
+            {
+                object? obj = System.Text.Json.JsonSerializer.Deserialize<object>(fs);
+                return Json(obj);
+            }
+        }
+    }
+}
---
+++ Day7-hw/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day7_hw.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
